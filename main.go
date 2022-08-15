d8a99e7786e5d38fc2139e10b4ea6027e0a5a391
Ed Foh <edwin.foh@gmail.com>
2022-08-15 22:46:26 +1000

    feat: implemented online election

--- main.go
+++ main.go
@@ -1,11 +1,94 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
 }
 
-func SomeFunc(x, y int) int {
-	return x + y
+/*
+Online Election https://leetcode.com/problems/online-election/
+
+You are given two integer arrays persons and times. In an election, the ith vote was cast for persons[i] at time times[i].
+
+For each query at a time t, find the person that was leading the election at time t. Votes cast at time t will count towards our query. In the case of a tie, the most recent vote (among tied candidates) wins.
+
+Implement the TopVotedCandidate class:
+
+TopVotedCandidate(int[] persons, int[] times) Initializes the object with the persons and times arrays.
+int q(int t) Returns the number of the person that was leading the election at time t according to the mentioned rules.
+
+persons: [0, 1, 1, 0, 0, 1, 0]. times: [0, 5, 10, 15, 20, 25, 30]
+*/
+type TopVotedCandidate struct {
+	times                []int
+	leadingPersonPerTime []int
+	totalVotesByPerson   map[int]int //key: person, calue number of votes
+}
+
+func NewTopVotedCandidate(persons []int, times []int) *TopVotedCandidate {
+	currentLeader := persons[0]
+	leadingPersonPerTime := make([]int, len(times))
+	totalVotesByPerson := make(map[int]int)
+
+	// assume lens are same
+	for i, person := range persons {
+		if _, ok := totalVotesByPerson[person]; !ok {
+			totalVotesByPerson[person] = 0
+		}
+		totalVotesByPerson[person] += 1 // increase persons vote by 1
+
+		// find current leader
+		if totalVotesByPerson[person] >= totalVotesByPerson[currentLeader] {
+			currentLeader = person
+		}
+		leadingPersonPerTime[i] = currentLeader
+	}
+
+	return &TopVotedCandidate{
+		leadingPersonPerTime: leadingPersonPerTime,
+		totalVotesByPerson:   totalVotesByPerson,
+		times:                times,
+	}
+}
+
+// Q Returns the number of the person that was leading the election at time t according to the mentioned rules.
+func (c *TopVotedCandidate) Q(t int) int {
+	timeIndex := c.binarySearchTimes(c.times, 0, len(c.times)-1, t)
+	if timeIndex != -1 {
+		return c.leadingPersonPerTime[timeIndex]
+	}
+	return -1
+}
+
+func (c *TopVotedCandidate) binarySearchTimes(times []int, low int, high int, t int) int {
+	if low <= high {
+		mid := (low + high) / 2
+
+		midVal := times[mid]
+
+		if midVal == t {
+			return mid
+		}
+
+		// when low == high, mid is same value as either. If midVal check fails, we default to the prev value
+		if low == high {
+			return min(low - 1)
+		}
+
+		if t < midVal {
+			return c.binarySearchTimes(times, low, mid-1, t)
+		}
+		return c.binarySearchTimes(times, mid+1, high, t)
+	}
+	return -1
+}
+
+func min(x int) int {
+	if x < 0 {
+		return 0
+	}
+	return x
 }
--- main_test.go
+++ main_test.go
@@ -6,22 +6,64 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSomeFunc(t *testing.T) {
-	type args struct {
-		x int
-		y int
-	}
+func TestTopVotedCandidate_Q(t *testing.T) {
+	persons := []int{0, 1, 1, 0, 0, 1, 0}
+	times := []int{0, 5, 10, 15, 20, 25, 30}
 	tests := []struct {
-		name string
-		args args
-		want int
+		name       string
+		persons    []int
+		times      []int
+		queryTime  int
+		wantPerson int
 	}{
-		// TODO: Add test cases.
+		{
+			name:       "test - query time 3",
+			persons:    persons,
+			times:      times,
+			queryTime:  3,
+			wantPerson: 0,
+		},
+		{
+			name:       "test - query time 12",
+			persons:    persons,
+			times:      times,
+			queryTime:  12,
+			wantPerson: 1,
+		},
+		{
+			name:       "test - query time 25",
+			persons:    persons,
+			times:      times,
+			queryTime:  25,
+			wantPerson: 1,
+		},
+		{
+			name:       "test - query time 15",
+			persons:    persons,
+			times:      times,
+			queryTime:  15,
+			wantPerson: 0,
+		},
+		{
+			name:       "test - query time 24",
+			persons:    persons,
+			times:      times,
+			queryTime:  24,
+			wantPerson: 0,
+		},
+		{
+			name:       "test - query time 8",
+			persons:    persons,
+			times:      times,
+			queryTime:  8,
+			wantPerson: 1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := SomeFunc(tt.args.x, tt.args.y)
-			assert.Equal(t, tt.want, got)
+			sut := NewTopVotedCandidate(tt.persons, tt.times)
+			gotPerson := sut.Q(tt.queryTime)
+			assert.Equal(t, tt.wantPerson, gotPerson)
 		})
 	}
 }
