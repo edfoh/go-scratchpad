717730b6f70a5f794a9d1f11f90d085bac05173b
Ed Foh <edwin.foh@gmail.com>
2022-08-15 23:29:22 +1000

    feat: lru cache

--- main.go
+++ main.go
@@ -1,11 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"container/list"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
 }
 
-func SomeFunc(x, y int) int {
-	return x + y
+/*
+https://leetcode.com/problems/lru-cache/
+
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+*/
+type LRUCache struct {
+	list     *list.List
+	nodes    map[int]*list.Element
+	capacity int
+}
+
+type keyValue struct {
+	Key   int
+	Value int
+}
+
+func newKeyValue(key, value int) *keyValue {
+	return &keyValue{key, value}
+}
+
+func NewLRUCache(capacity int) *LRUCache {
+	return &LRUCache{
+		list:     list.New(),
+		nodes:    map[int]*list.Element{},
+		capacity: capacity,
+	}
+}
+
+func (cache *LRUCache) Get(key int) int {
+	res := -1
+	if ele, ok := cache.nodes[key]; ok {
+		kv := ele.Value.(*keyValue)
+		res = kv.Value
+		cache.list.MoveToFront(ele)
+	}
+	return res
+}
+
+func (cache *LRUCache) Put(key int, value int) {
+	// if key exist, update value and move to front of linked list
+	if ele, ok := cache.nodes[key]; ok {
+		updateValue(ele, value)
+		cache.list.MoveToFront(ele)
+	} else {
+		// if not found, add new node and update map
+		cache.nodes[key] = cache.list.PushFront(newKeyValue(key, value))
+
+		// check if capacity has been reached, if so remove last element
+		if cache.list.Len() > cache.capacity {
+			cache.removeLast()
+		}
+	}
+}
+
+func updateValue(ele *list.Element, value int) {
+	kv := ele.Value.(*keyValue)
+	kv.Value = value
+}
+
+func (cache *LRUCache) removeLast() {
+	lastEle := cache.list.Back()
+	lastKv := lastEle.Value.(*keyValue)
+	delete(cache.nodes, lastKv.Key)
+	cache.list.Remove(lastEle)
 }
--- main_test.go
+++ main_test.go
@@ -6,22 +6,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSomeFunc(t *testing.T) {
-	type args struct {
-		x int
-		y int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := SomeFunc(tt.args.x, tt.args.y)
-			assert.Equal(t, tt.want, got)
-		})
-	}
+func TestLRUCache(t *testing.T) {
+	lru := NewLRUCache(2)
+	lru.Put(1, 1)
+	lru.Put(2, 2) // cache is {1=1, 2=2}
+	assert.Equal(t, 1, lru.Get(1))
+	lru.Put(3, 3) // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+	assert.Equal(t, -1, lru.Get(2))
+	lru.Put(4, 4) // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+	assert.Equal(t, -1, lru.Get(1))
+	assert.Equal(t, 3, lru.Get(3))
+	assert.Equal(t, 4, lru.Get(4))
 }
