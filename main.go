91221d1f8f1b2d93ba19811c5705d6860b671b29
Ed Foh <edwin.foh@gmail.com>
2022-08-17 00:01:43 +1000

    feat: k-largest

--- main.go
+++ main.go
@@ -1,11 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
 }
 
-func SomeFunc(x, y int) int {
-	return x + y
+/*
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+You must solve it in O(n) time complexity.
+*/
+
+func FindKthLargest(nums []int, k int) int {
+	return kLargest(&nums, 0, len(nums)-1, k-1)
+}
+
+func findPartition(nums *[]int, low, high int) int {
+	pivot := high
+
+	pivotVal := (*nums)[pivot]
+
+	j := low
+
+	for i := low; i < pivot; i++ {
+		if (*nums)[i] > pivotVal {
+			(*nums)[i], (*nums)[j] = (*nums)[j], (*nums)[i]
+			j++
+		}
+	}
+
+	(*nums)[j], (*nums)[pivot] = (*nums)[pivot], (*nums)[j]
+	return j
+}
+
+func kLargest(nums *[]int, low, high, k int) int {
+	pi := findPartition(nums, low, high)
+
+	if pi == k {
+		return (*nums)[pi]
+	}
+
+	if pi > k {
+		return kLargest(nums, low, pi-1, k)
+	}
+
+	return kLargest(nums, pi+1, high, k)
+
 }
--- main_test.go
+++ main_test.go
@@ -6,22 +6,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSomeFunc(t *testing.T) {
-	type args struct {
-		x int
-		y int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
+func TestFindKthLargest(t *testing.T) {
+	testCases := []struct {
+		desc      string
+		nums      []int
+		k         int
+		wantValue int
 	}{
-		// TODO: Add test cases.
+		{
+			desc:      "input 1",
+			nums:      []int{3, 2, 1, 5, 6, 4},
+			k:         2,
+			wantValue: 5,
+		},
+		{
+			desc:      "input 2",
+			nums:      []int{3, 2, 3, 1, 2, 4, 5, 5, 6},
+			k:         4,
+			wantValue: 4,
+		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := SomeFunc(tt.args.x, tt.args.y)
-			assert.Equal(t, tt.want, got)
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			gotValue := FindKthLargest(tC.nums, tC.k)
+			assert.Equal(t, tC.wantValue, gotValue)
 		})
 	}
 }
