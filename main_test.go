38a7d44405ef2ddac7f5779c7314b2c1ebd49af7
Ed Foh <edwin.foh@gmail.com>
2022-08-17 23:38:28 +1000

    feat: calculator

--- main_test.go
+++ main_test.go
@@ -6,22 +6,37 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSomeFunc(t *testing.T) {
-	type args struct {
-		x int
-		y int
-	}
+func TestCalculate(t *testing.T) {
 	tests := []struct {
-		name string
-		args args
-		want int
+		name    string
+		exp     string
+		wantRes int
 	}{
-		// TODO: Add test cases.
+		{
+			name:    "expr1",
+			exp:     "3+2*2",
+			wantRes: 7,
+		},
+		{
+			name:    "expr2",
+			exp:     "3 / 2",
+			wantRes: 1,
+		},
+		{
+			name:    "expr3",
+			exp:     "3+5 / 2",
+			wantRes: 5,
+		},
+		{
+			name:    "expr4",
+			exp:     "3*5-6/2",
+			wantRes: 12,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := SomeFunc(tt.args.x, tt.args.y)
-			assert.Equal(t, tt.want, got)
+			gotRes := Calculate(tt.exp)
+			assert.Equal(t, tt.wantRes, gotRes)
 		})
 	}
 }
