d3cfc853bfc2e377f766098ddef3e9d6483fce66
Ed Foh <edwin.foh@gmail.com>
2022-08-18 23:50:45 +1000

    revert main

--- main_test.go
+++ main_test.go
@@ -1,42 +1,18 @@
 package main
 
-import (
-	"testing"
+import "testing"
 
-	"github.com/stretchr/testify/assert"
-)
-
-func TestCalculate(t *testing.T) {
-	tests := []struct {
-		name    string
-		exp     string
-		wantRes int
+func Test(t *testing.T) {
+	testCases := []struct {
+		desc string
 	}{
 		{
-			name:    "expr1",
-			exp:     "3+2*2",
-			wantRes: 7,
-		},
-		{
-			name:    "expr2",
-			exp:     "3 / 2",
-			wantRes: 1,
-		},
-		{
-			name:    "expr3",
-			exp:     "3+5 / 2",
-			wantRes: 5,
-		},
-		{
-			name:    "expr4",
-			exp:     "3*5-6/2",
-			wantRes: 12,
+			desc: "",
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotRes := Calculate(tt.exp)
-			assert.Equal(t, tt.wantRes, gotRes)
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+
 		})
 	}
 }
